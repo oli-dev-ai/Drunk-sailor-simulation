6c4059e8c87ddfba35977c1fc116a38d980acef8
oli-dev-ai <oopyrchal@pm.me>
2026-01-05 20:48:34 +0100

    10 DRUNKS SAILORS - GAUSS

---
+++ sim_gauss.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+rng = np.random.default_rng()
+
+#Array of sailor steps
+sailor_steps = rng.standard_normal((1000,10))
+
+#Cumulative steps
+position = np.cumsum(sailor_steps, axis=0)
+
+mean_pos = np.mean(position[-1])
+print(mean_pos)
+
+#Chart
+plt.plot(position, label='sailor')
+plt.axhline(y=0, color='k', linestyle='--')
+plt.xlabel('steps')
+plt.ylabel('position')
+plt.title('10 Drunk sailors simulation - Gauss')
+plt.show()
