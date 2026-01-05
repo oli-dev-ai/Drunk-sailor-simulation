d9626611179004e5f379004896ab9d4b91736fbd
oli-dev-ai <oopyrchal@pm.me>
2026-01-05 19:44:49 +0100

    10 DRUNKS SAILORS :D

--- sim.py
+++ sim.py
@@ -4,11 +4,11 @@ rng = np.random.default_rng()
 
 
 #Array of sailor steps
-sailor_steps = rng.integers(0, 2, size=1000)
+sailor_steps = rng.integers(0, 2, size=(10,1000))
 sailor_steps[sailor_steps == 0] = -1
 
 #Cumulative steps
-position = np.cumsum(sailor_steps)
+position = np.cumsum(sailor_steps, axis=1)
 #Highest value
 maximal_step = np.max(position)
 #Lowest value
@@ -16,10 +16,10 @@ lowest_step = np.min(position)
 
 
 #Chart
-plt.plot(position, label='sailor')
+plt.plot(position.T, label='sailor')
 plt.axhline(y=0, color='k', linestyle='--')
 plt.xlabel('steps')
 plt.ylabel('position')
-plt.title('Drunk sailor simulation')
+plt.title('10 Drunk sailors simulation')
 plt.show()
 
